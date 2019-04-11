798e37d50d75d9b287bc517f4c9dd2cfb8bd0f4b
Sajidk25 <sajidk25@gmail.com>
2019-04-11 18:03:42 +0600

    latest features/bootstrap/FeatureContext.php

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -18,5 +18,56 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+        $this->bearerToken = "";
+    }
+
+    /**
+     * @Given I have the payload:
+     */
+    public function iHaveThePayload(PyStringNode $string)
+    {
+        $this->payload = $string;
+    }
+
+    /**
+     * @When /^I request "(GET|PUT|POST|DELETE|PATCH) ([^"]*)"$/
+     */
+    public function iRequest($httpMethod, $argument1)
+    {
+        $client = new GuzzleHttp\Client();
+        $this->response = $client->request(
+            $httpMethod,
+            'http://127.0.0.1:8000' . $argument1,
+            [
+                'body' => $this->payload,
+                'headers' => [
+                    "Authorization" => "Bearer {$this->bearerToken}",
+                    "Content-Type" => "application/json",
+                ],
+            ]
+        );
+        $this->responseBody = $this->response->getBody(true);
+    }
+
+    /**
+     * @Then /^I get a response$/
+     */
+    public function iGetAResponse()
+    {
+        if (empty($this->responseBody)) {
+            throw new Exception('Did not get a response from the API');
+        }
+    }
+
+    /**
+     * @Given /^the response is JSON$/
+     */
+    public function theResponseIsJson()
+    {
+        $data = json_decode($this->responseBody);
+
+        if (empty($data)) {
+            throw new Exception("Response was not JSON\n" . $this->responseBody);
+        }
     }
 }
